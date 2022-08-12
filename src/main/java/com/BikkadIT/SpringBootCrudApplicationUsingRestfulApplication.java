646570363645447a3713c65c59334827add3705a
Pravin <pravinbhakar01@gmail.com>
2022-08-12 11:50:30 +0530

    SBCWR-1 :Create project and required package structure

---
+++ src/main/java/com/BikkadIT/SpringBootCrudApplicationUsingRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudApplicationUsingRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudApplicationUsingRestfulApplication.class, args);
+	}
+
+}
